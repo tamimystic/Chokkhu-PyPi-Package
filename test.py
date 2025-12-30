482c875336f76aaccf59780e85f9e8f0379a78c3
tamimystic <tamimhossainforcoding01@gmail.com>
2025-12-30 23:41:52 +0600

    add custom exception handling for invalid URLs and implement logging

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 from Chokkhu.logger import logger
+from Chokkhu.custom_exception import InvalidURLException
 
 logger.info("this is testing log")
+
+try:
+    raise InvalidURLException()
+except Exception as e:
+    logger.error(f"Cought an exception: {str(e)}")
